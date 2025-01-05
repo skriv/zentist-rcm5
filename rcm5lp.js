323329c869274c96f796d94e299f74b88d1b570e
Alex Krivov <skriv80@gmail.com>
2025-01-05 19:21:10 +0600

    Fix Coundown

--- rcm5lp.js
+++ rcm5lp.js
@@ -1,24 +1,25 @@
 console.log("Hello RCM 5");
-const deadline = "2025/09/13 00:00";
-let swiper;
-var typeTicket = $("input[name='Type']");
-var campusSelect = true;
-var dsoSelect = true;
+const eventDate = new Date("September 13, 2025"); // target date
+const ONLINE = $('form[data-commerce-product-id="6778a1eeed0c7c259255aac4"]');
+const CAMPUS = $('form[data-commerce-product-id="6778a1a71ac6cc156adc2e47"]');
 
-var ONLINE = $('form[data-commerce-product-id="6778a1eeed0c7c259255aac4"]');
-var CAMPUS = $('form[data-commerce-product-id="6778a1a71ac6cc156adc2e47"]');
+let swiper;
+let typeTicket = $("input[name='Type']");
+let campusSelect = true;
+let dsoSelect = true;
+
+
 
 var buttonInOnlineForm = ONLINE.find('a[data-node-type="commerce-buy-now-button"]');
 var buttonInCampusForm = CAMPUS.find('a[data-node-type="commerce-buy-now-button"]'); 
 
-
+ticketName();
 
 $(document).ready(function () {
   console.log("RCM5 is ready");
-  countDown("js-clock", deadline);
-  ticketName();
-  addAttendeeRows(1);
+  addAttendeeRows(1); // Добавляем одну строку при загрузке страницы
   players();
+  countdown();
 });
 
 function players() {
@@ -28,11 +29,17 @@ function players() {
         if (videoSrc) {
             const player = videojs(videoElement, {
                 aspectRatio: '16:9',
+                controls: true,
+                autoplay: false,
+                preload: 'auto',
+                fluid: true,
                 techOrder: ["youtube"],
                 sources: [{ type: "video/youtube", src: videoSrc }],
                 youtube: {
                     modestbranding: 1,
-                    rel: 0
+                    rel: 0,
+                    enablejsapi: 1,
+                    showinfo: 0
                 }
             });
             players.push(player);
@@ -43,83 +50,54 @@ function players() {
 
     players.forEach((player, index) => {
         player.on('play', () => {
+            swiper.slideTo(index);
             players.forEach((otherPlayer, otherIndex) => {
                 if (index !== otherIndex) {
                     otherPlayer.pause();
                 }
             });
         });
-
-        player.on('seeked', () => {
-            console.log(`Video ${index} перемотано на ${player.currentTime()} секунд`);
-        });
     });
 }
 
 // VIDEO SLIDER
 $(".slider-main_component").each(function () {
-  let sliderDuration = +$(this).attr("slider-duration") || 300;
-  swiper = new Swiper($(this).find(".swiper")[0], {
-    speed: sliderDuration,
-    loop: false,
-    autoHeight: false,
-    followFinger: false,
-    freeMode: false,
-    slideToClickedSlide: false,
-    slidesPerView: 1,
-    spaceBetween: "4%",
-    rewind: false,
-    mousewheel: false,
-    keyboard: {
-      enabled: false,
-      onlyInViewport: false,
-    },
-    touchMove: false,
-    breakpoints: {
-      480: { slidesPerView: 1, spaceBetween: "4%" },
-      768: { slidesPerView: 2, spaceBetween: "4%" },
-      992: { slidesPerView: 1.5, spaceBetween: 32 },
-    },
-    navigation: {
-      nextEl: $(this).find(".swiper-next")[0],
-      prevEl: $(this).find(".swiper-prev")[0],
-      disabledClass: "is-disabled",
-    },
-    slideActiveClass: "is-active",
-    slideDuplicateActiveClass: "is-active",
-    initialSlide: 1,
-    centeredSlides: true,
-  });
+    let sliderDuration = +$(this).attr("slider-duration") || 300; // Упрощение
+    swiper = new Swiper($(this).find(".swiper")[0], {
+      speed: sliderDuration,
+      loop: false,
+      autoHeight: false,
+      followFinger: false,
+      freeMode: false,
+      slideToClickedSlide: false,
+      slidesPerView: 1,
+      spaceBetween: "4%",
+      rewind: false,
+      mousewheel: false,
+      keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+      },
+      breakpoints: {
+        480: { slidesPerView: 1, spaceBetween: "4%" },
+        768: { slidesPerView: 2, spaceBetween: "4%" },
+        992: { slidesPerView: 1.5, spaceBetween: 32 },
+      },
+      navigation: {
+        nextEl: $(this).find(".swiper-next")[0],
+        prevEl: $(this).find(".swiper-prev")[0],
+        disabledClass: "is-disabled",
+      },
+      slideActiveClass: "is-active",
+      slideDuplicateActiveClass: "is-active",
+      initialSlide: 1,
+      centeredSlides: true,
+    });
+
+    $(this).find('.swiper-slide').on('click', function () {
+        swiper.slideTo($(this).index());
+    });
 });
-
-// COUNTDOWN
-function pad(num, size) {
-  return ("0" + num).slice(-size);
-}
-
-function getTimeRemaining(endtime) {
-  var t = Date.parse(endtime) - Date.parse(new Date());
-  return {
-    total: t,
-    days: Math.floor(t / (1000 * 60 * 60 * 24)),
-    hours: Math.floor((t / (1000 * 60 * 60)) % 24),
-    minutes: Math.floor((t / 1000 / 60) % 60),
-    seconds: Math.floor((t / 1000) % 60),
-  };
-}
-
-function countDown(id, endtime) {
-  var timeinterval = setInterval(function () {
-    var t = getTimeRemaining(endtime);
-    if (t.total <= 0) {
-      clearInterval(timeinterval);
-    } else {
-      ["days", "hours", "minutes", "seconds"].forEach(unit => {
-        document.getElementById(`${id}-${unit}`).innerHTML = pad(t[unit], 2);
-      });
-    }
-  }, 1000);
-}
 
 // SELECT PLAN OPTION
 $(".rcm-plan4-wrapper").click(function () {
@@ -140,37 +118,75 @@ function ticketName() {
 function addAttendeeRows(quantity) {
     $(".add-info").empty();
     for (var i = 0; i < quantity; i++) {
-      $(".add-info").append(
-        `<div class='attendee-row'>
-          <input type='text' class='form-text-rcm4 white w-input' maxlength='256' name='Attendee ${i}' data-name='Attendee Name ${i}' placeholder='Name' id='attendee ${i}' required>
-          <input type='email' class='form-text-rcm4 white w-input' maxlength='256' name='email ${i}' data-name='Attendee Email ${i}' placeholder='Email' id='email ${i}' required>
-          <input type='phone' class='form-text-rcm4 white w-input' maxlength='256' name='phone ${i}' data-name='Attendee Phone ${i}' placeholder='Phone' id='phone ${i}' required>
-        </div>`
-      );
+        var str = `<div class='attendee-row'>
+            <input type='text' class='form-text-rcm4 white w-input' maxlength='256' name='Attendee ${i}' data-name='Attendee Name ${i}' placeholder='Name' id='attendee ${i}' required>
+            <input type='email' class='form-text-rcm4 white w-input' maxlength='256' name='email ${i}' data-name='Attendee Email ${i}' placeholder='Email' id='email ${i}' required>
+            <input type='phone' class='form-text-rcm4 white w-input' maxlength='256' name='phone ${i}' data-name='Attendee Phone ${i}' placeholder='Phone' id='phone ${i}' required>
+        </div>`;
+        $(".add-info").append(str);
     }
 }
 
 // Обновляем строки при изменении значения поля ввода
 $(".rcm-4-quantity").on("input", function () {
-  var quantity = parseInt($(this).val());
-  addAttendeeRows(quantity);
+    var quantity = parseInt($(this).val());
+    addAttendeeRows(quantity);
 });
 
 // Выбор формы для отслеживания события submit
-var form = $(".my-form");
+let form = $(".my-form");
 form.on("submit", function (event) {
     event.preventDefault();
     if (dsoSelect) {
-      (campusSelect ? buttonInCampusForm : buttonInOnlineForm)[0].click();
+        (campusSelect ? buttonInCampusForm : buttonInOnlineForm)[0].click();
     } else {
-      $("#registration-wrapper").hide();
-      $(".success-message-3 div").html(
-        "<div class='text-block-154 text-balanced'>The bootcamp attendance is reserved to Dental Support Organizations DSOs. <br/>For sponsorship opportunities, please send us an email <a href='mailto:growth@zentist.io'>growth@zentist.io</a>.</div>"
-      );
+        $("#registration-wrapper").hide();
+        $(".success-message-3 div").html("<div class='text-block-154 text-balanced'>The bootcamp attendance is reserved to Dental Support Organizations DSOs. <br/>For sponsorship opportunities, please send us an email <a href='mailto:growth@zentist.io'>growth@zentist.io</a>.</div>");
     }
     var organizationName = $("#Organization").val();
     console.log(organizationName);
     Cookies.set("myCookie", organizationName, { expires: 365 });
 });
+
+
+function countdown() {
+    console.log("countdown");
+  
+    // Function to add leading zero
+    function addLeadingZero(number) {
+      return number < 10 ? "0" + number : number;
+    }
+  
+    // Function to update the countdown
+    function updateCountdown() {
+      let now = new Date(); // current date and time
+  
+      let currentTime = now.getTime();
+      let eventTime = eventDate.getTime();
+  
+      let remainingTime = eventTime - currentTime;
+  
+      let seconds = Math.floor(remainingTime / 1000);
+      let minutes = Math.floor(seconds / 60);
+      let hours = Math.floor(minutes / 60);
+      let days = Math.floor(hours / 24);
+  
+      hours %= 24;
+      minutes %= 60;
+      seconds %= 60;
+  
+      // Update the elements with the calculated and formatted time
+      $("#days").text(addLeadingZero(days));
+      $("#hours").text(addLeadingZero(hours));
+      $("#minutes").text(addLeadingZero(minutes));
+      $("#seconds").text(addLeadingZero(seconds)); // Update seconds with leading zero
+  
+      // Recursive call every second
+      setTimeout(updateCountdown, 1000);
+    }
+  
+    // Initial call to start the countdown
+    updateCountdown();
+}
 
 
