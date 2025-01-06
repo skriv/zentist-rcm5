713d3e9696a97e4eb5c78ef8546971cd2414e3c3
Alex Krivov <skriv80@gmail.com>
2025-01-06 17:25:47 +0600

    2222

--- test.js
+++ test.js
@@ -59,10 +59,10 @@ const swiper = new Swiper('.swiper', {
     loop: false,
     centeredSlides: true, // Центрирование активного слайда
     // slidesPerView: 1.5, // Видимые слайды
-    // spaceBetween: 32,
+    spaceBetween: 32,
     speed: 400,
     breakpoints: {
-        480: { slidesPerView: 2, spaceBetween: 20 },
+        480: { slidesPerView: 2, spaceBetween: 20, followFinger: true },
         768: { slidesPerView: 2, spaceBetween: 20 },
         992: { slidesPerView: 1.5, spaceBetween: 32 },
       },
