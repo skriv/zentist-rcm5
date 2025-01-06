0d8dac0917bd5ff9a26c8379bf70d609e8cc9e96
Alex Krivov <skriv80@gmail.com>
2025-01-06 17:27:39 +0600

    33333

--- test.js
+++ test.js
@@ -62,7 +62,7 @@ const swiper = new Swiper('.swiper', {
     spaceBetween: 32,
     speed: 400,
     breakpoints: {
-        480: { slidesPerView: 2, spaceBetween: 20, followFinger: true },
+        480: { slidesPerView: 2, spaceBetween: 20, followFinger: true, freeMode: true },
         768: { slidesPerView: 2, spaceBetween: 20 },
         992: { slidesPerView: 1.5, spaceBetween: 32 },
       },
