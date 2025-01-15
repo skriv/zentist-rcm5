0106777bafa46b400f2b0f23e92b07e284b3046e
Alex Krivov <skriv80@gmail.com>
2025-01-15 11:11:20 +0600

    Added V3

--- rcm5checkout.js
+++ rcm5checkout.js
@@ -1,11 +1,19 @@
 $(document).ready(function () {
 
-    var cookieValue = Cookies.get('myCookie'); // Чтение значения cookie
-    
-    if (cookieValue) {
-        $('#company').val(cookieValue); // Установка значения в текстовое поле
-    }else{
-        console.log("tttt")
+    // Функция для установки значения в текстовое поле и логирования
+    function setValueAndLog(cookieName, elementId, noValueMessage) {
+        let cookieValue = Cookies.get(cookieName);
+        if (cookieValue) {
+            $(elementId).val(cookieValue); // Установка значения в текстовое поле
+        } else {
+            console.log(noValueMessage);
+        }
     }
+
+    // Установка значений для различных полей
+    setValueAndLog('org', '#company', "NO ORGANIZATION");
+    setValueAndLog('info', '#information', "NO ATTENDEES");
+    setValueAndLog('locations', '#locations', "NO LOCATIONS");
+    setValueAndLog('preEvent', '#preevent', "NO PRE EVENT");
 
 });
